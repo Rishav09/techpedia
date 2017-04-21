bcbdb1a0ef0ca3e16bd41c3bbe9b8d6127e96ecd
rishav09 <rishavsapahia@gmail.com>
2017-04-21 14:17:33 +0530

    Before configuring database

--- techpedia/views.py
+++ techpedia/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	
+	context_dict={}
+	return render(request,'techpedia/index.html',context=context_dict)
+
+def about(request):
+	return 
 
--- techpedia_project/urls.py
+++ techpedia_project/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 from techpedia import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$',views.index,name='index'),
     url(r'^techpedia/',include('techpedia.urls')),
-]
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
